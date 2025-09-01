cf1c4a8e0e240ecafc815d542e4c2e0819b411c9
Sylvain Galoustoff <sylvain.galoustoff@gmail.com>
2025-09-01 11:12:59 +0200

    Set preview

--- client/src/views/AddProject/AddProject.tsx
+++ client/src/views/AddProject/AddProject.tsx
@@ -1,10 +1,11 @@
-import { Button, Input, Select, DatePicker } from "melogems";
+import { Button, Input, Select, DatePicker, Avatar } from "melogems";
 import styles from "./AddProject.module.css";
 import { IoPerson, IoCheckmark } from "react-icons/io5";
 import { useEffect, useState } from "react";
 import type { User } from "@meloprojects/shared";
 import { fetchData } from "../../helpers/apiCalls";
 import { useToaster } from "../../context/ToasterContext";
+import { useAuth } from "../../context/AuthContext";
 
 function AddProject() {
   const [projectName, setProjectName] = useState<string>("");
@@ -12,6 +13,7 @@ function AddProject() {
   const [deadline, setDeadline] = useState<string>("");
   const [options, setOptions] = useState<string[]>([]);
   const { addToast } = useToaster();
+  const { user } = useAuth();
 
   useEffect(() => {
     const getUsers = async () => {
@@ -66,6 +68,34 @@ function AddProject() {
           />
         </div>
       </form>
+      <div className={styles.preview}>
+        <div className={`form-group ${styles.previewGroup}`}>
+          <p className={`label`}>Votre projet :</p>
+
+          <div className={`card ${styles.previewCard}`}>
+            <div className={styles.cardHeader}>
+              <h2 className={styles.h2}>
+                {projectName.length > 0 ? projectName : "Nom du projet"}
+              </h2>
+              <p className={`help`}>
+                A terminer avant : {deadline.length > 0 ? deadline : "non défini"}
+              </p>
+            </div>
+            <div className={styles.cardBody}>
+              <h3 className={styles.h3}>Assignés au projet :</h3>
+              <div className={styles.avatars}>
+                {user !== undefined && (
+                  <div className={styles.owner}>
+                    <p className={`help`}>Superviseur :</p>
+                    <Avatar name={user.displayName ? user.displayName : user.name} />
+                  </div>
+                )}
+                {users.length > 0 && <Avatar name={"Bernard"} />}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
