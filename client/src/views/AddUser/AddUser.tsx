0097339f32cdcd93674ed78367d88a52c666295a
Sylvain Galoustoff <sylvain.galoustoff@gmail.com>
2025-09-14 07:42:56 +0200

    Add form to page

--- client/src/views/AddUser/AddUser.tsx
+++ client/src/views/AddUser/AddUser.tsx
@@ -1,7 +1,88 @@
+import { Input, Button } from "melogems";
 import styles from "./AddUser.module.css";
+import { useState } from "react";
+import { postData } from "../../helpers/apiCalls";
+import { useToaster } from "../../context/ToasterContext";
+import { useNavigate } from "react-router";
 
 function AddUser() {
-  return <div className={`page ${styles.addUser}`}>Add user page</div>;
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [confirm, setConfirm] = useState<string>("");
+  const { addToast } = useToaster();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (password === confirm) {
+      const response = await postData("/users/register", {
+        name: username,
+        password,
+      });
+
+      if (response.message === "Un utilisateur avec ce nom existe déjà") {
+        addToast({
+          variant: "danger",
+          header: "Erreur",
+          message: "L'utilisateur existe déjà.",
+        });
+      } else if (response.message === "Utilisateur enregistré avec succès") {
+        addToast({
+          variant: "success",
+          header: "Enregistrement terminé",
+          message: `${username} a été ajouté.`,
+        });
+        navigate("/login");
+      } else if (
+        response.message === "Erreur lors de l'enregistrement de l'utilisateur"
+      ) {
+        addToast({
+          variant: "danger",
+          header: "Erreur",
+          message: "Erreur serveur",
+        });
+      }
+    }
+  };
+
+  return (
+    <div className={`page ${styles.addUser}`}>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <div className={`form-group`}>
+          <label htmlFor="identifiant">Prénom</label>
+          <Input
+            type={"text"}
+            value={username}
+            onChange={(value) => setUsername(value)}
+          />
+        </div>
+        <div className={`form-group`}>
+          <label htmlFor="password">Mot de passe</label>
+          <Input
+            type={"password"}
+            value={password}
+            onChange={(value) => setPassword(value)}
+          />
+        </div>
+        <div className={`form-group`}>
+          <label htmlFor="confirm">Confirmez le mot de passe</label>
+          <Input
+            type={"password"}
+            value={confirm}
+            onChange={(value) => setConfirm(value)}
+          />
+        </div>
+        <div className={`form-group button-group`}>
+          <Button
+            type="submit"
+            label={"Enregistrer"}
+            variant={password.length > 3 && password === confirm ? "primary" : "disabled"}
+          />
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default AddUser;
